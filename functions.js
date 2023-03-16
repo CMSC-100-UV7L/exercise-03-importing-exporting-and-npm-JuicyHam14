99121510ddd47db24d08d9c6989a198bfb03125d
JuicyHam14 <jbcortes2@up.edu.ph>
2023-03-17 05:32:25 +0800

    feat: import/export enabled

---
+++ functions.js
@@ -0,0 +1,14 @@
+// Cortes, Jared B.     2018-03612     UV-7L        03/17/23
+// Exercise 3: Javascript files implementing importing and exporting along
+// with the use of NPM package manager.
+
+
+function generateUniqueID(){
+    console.log("generated unique id!");
+}
+
+function addAccount(){
+    console.log("added account!");
+}
+
+export { generateUniqueID, addAccount }
---
+++ test.js
@@ -0,0 +1,9 @@
+// Cortes, Jared B.     2018-03612     UV-7L        03/17/23
+// Exercise 3: Javascript files implementing importing and exporting along
+// with the use of NPM package manager.
+
+
+import { generateUniqueID, addAccount } from "./functions.js";
+
+generateUniqueID();
+addAccount();
